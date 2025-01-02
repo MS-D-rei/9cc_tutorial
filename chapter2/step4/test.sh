37d0b7426d28b50cb6a22e4a2da269f03d76736f
MS_D_rei <msmdmission1111@gmail.com>
2025-01-02 18:47:09 +0900

    Created: chapter2/step4, error_at function file

---
+++ chapter2/step4/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# if want to debug, run `bash -x test.sh`
+
+assert() {
+    input="$1"
+    expected="$2"
+
+    ./9cc "$input" > temp.s
+    cc -o temp temp.s
+    ./temp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+    	echo "$input => $expected expected, but got $actual"
+	exit 1
+    fi
+}
+
+assert 0+0 0
+assert 20+5-4 21
+assert " 20 + 5 - 4" 21
+
+echo "Test end"
