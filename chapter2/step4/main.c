913e6b1755d75b9e832d560190d3ba466e978d4a
MS_D_rei <msmdmission1111@gmail.com>
2025-01-03 00:07:32 +0900

    Separated: tokenizer code from main.c

--- chapter2/step4/main.c
+++ chapter2/step4/main.c
@@ -1,24 +1,9 @@
-#include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "error_at.h"
-
-typedef enum {
-    TK_RESERVED,
-    TK_NUM,
-    TK_EOF,
-} TokenKind;
-
-typedef struct Token Token;
-struct Token {
-    TokenKind kind;
-    Token* next;
-    int val;
-    char* str;
-};
+#include "tokenizer.h"
 
 char* user_input;
 
@@ -32,66 +17,6 @@ void error(char* fmt, ...) {
     exit(1);
 }
 
-/* token->kind == TK_RESERVED && token->str[0] == op */
-bool consume(char op) {
-    if (token->kind != TK_RESERVED || token->str[0] != op) {
-        return false;
-    }
-    token = token->next;
-    return true;
-}
-
-int expect_number() {
-    if (token->kind != TK_NUM) {
-        // error("not number");
-        error_at(user_input, token->str, "not number");
-    }
-    int val = token->val;
-    token = token->next;
-    return val;
-}
-
-bool at_eof() { return token->kind == TK_EOF; }
-
-Token* create_token(TokenKind kind, Token* current, char* str) {
-    Token* new_token = calloc(1, sizeof(Token));
-    new_token->kind = kind;
-    new_token->str = str;
-    current->next = new_token;
-    return new_token;
-}
-
-Token* tokenize() {
-    char* p = user_input;
-    Token head;
-    head.next = NULL;
-    Token* current = &head;
-
-    while (*p) {
-        if (isspace(*p)) {
-            p++;
-            continue;
-        }
-        if (*p == '+' || *p == '-') {
-            current = create_token(TK_RESERVED, current, p);
-            p++;
-            continue;
-        }
-        if (isdigit(*p)) {
-            current = create_token(TK_NUM, current, p);
-            current->val = strtol(p, &p, 10);
-            continue;
-        }
-
-        // error("cannot tokenize");
-        error_at(user_input, p, "cannot tokenize");
-    }
-
-    create_token(TK_EOF, current, p);
-
-    return head.next;
-}
-
 int main(int argc, char** argv) {
     if (argc != 2) {
         error("argument should be 1");
@@ -99,21 +24,27 @@ int main(int argc, char** argv) {
 
     user_input = argv[1];
 
-    token = tokenize();
+    token = tokenize(user_input);
+
+    // printf("%p\n", (void*) token);
+    // printf("%p\n", (void*) &token);
+    // printf("%p\n", (void*) token->next);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    printf("  mov rax, %d\n", expect_number());
+    printf("  mov rax, %d\n", expect_number(user_input, &token));
 
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
+    // printf("%p\n", (void*) token);
+
+    while (!at_eof(token)) {
+        if (consume(&token, '+')) {
+            printf("  add rax, %d\n", expect_number(user_input, &token));
             continue;
         }
-        if (consume('-')) {
-            printf("  sub rax, %d\n", expect_number());
+        if (consume(&token, '-')) {
+            printf("  sub rax, %d\n", expect_number(user_input, &token));
             continue;
         }
     }
