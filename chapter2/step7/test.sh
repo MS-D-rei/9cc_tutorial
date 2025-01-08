fb129321bbc576bb6a89adf0fedeb1aa1eab58d0
MS_D_rei <msmdmission1111@gmail.com>
2025-01-08 17:53:13 +0900

    Added: test code for unary and multi letter operator

--- chapter2/step7/test.sh
+++ chapter2/step7/test.sh
@@ -26,4 +26,17 @@ assert "5+6* 7" 47
 assert "5*(9-6)" 15
 assert "(3+5)/2" 4
 
+assert "-2 + 6" 4
+assert "+6 - 1" 5
+
+assert "1 == 1" 1
+assert "1 != 0" 1
+assert "1 != 1" 0
+assert "1+1 == 2" 1
+assert "(1+1)*2 == 4" 1
+assert "(1+1)*2 < 4" 0
+assert "(1+1)*2 <= 4" 1
+assert "(1+1)*2 > 4" 0
+assert "(1+1)*2 >= 4" 1
+
 echo "Test end"
