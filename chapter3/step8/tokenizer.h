5e6570388b5f33d7d47548588f8381c50e910e00
MS_D_rei <msmdmission1111@gmail.com>
2025-01-08 19:23:32 +0900

    Start: step8, copied files from step7

---
+++ chapter3/step8/tokenizer.h
@@ -0,0 +1,33 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+typedef enum {
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+struct Token {
+    TokenKind kind;
+    Token* next;
+    int val;
+    char* str;
+    int len;
+};
+
+bool consume_op(Token** token, char* op);
+
+void expect_op(char* user_input, Token** token, char* op);
+
+int expect_number(char* user_input, Token** token);
+
+bool at_eof(Token* token);
+
+Token* create_token(TokenKind kind, Token* current, char* str, int len);
+
+bool multiletter_op(char* op, char* p);
+
+Token* tokenize(char* user_input);
+
+#endif // !TOKENIZER_H
